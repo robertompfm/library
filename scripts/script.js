6cd488764586803115276b7b4d181867fccbd7e0
Roberto Mendes <robertompfm@gmail.com>
2020-09-05 11:53:06 -0300

    simple version complete

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,9 @@ const TABLE_BODY = document.querySelector(".table").querySelector("tbody");
 
 // ==== BOOK CLASS ====
 // CONSTRUCTOR
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
   // ATTRIBUTES
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -14,10 +15,18 @@ function Book(title, author, pages, read) {
 }
 
 // METHODS
+Book.prototype.getId = function() {
+  return this.id;
+}
+
 Book.prototype.info = function() {
   let readStr = this.read ? "read" : "not read yet";
-  let infoStr = `${this.title} by ${this.author}, ${this.pages} pages, ${readStr}`;
+  let infoStr = `${this.title} by ${this.author}, ${this.pages} pages, ${readStr}, id=${this.id}`;
   console.log(infoStr);
+}
+
+Book.prototype.toggleReadStatus = function() {
+  this.read = !this.read;
 }
 
 Book.prototype.render = function(tableBody) {
@@ -27,31 +36,35 @@ Book.prototype.render = function(tableBody) {
   let author = document.createElement("td");
   let pages = document.createElement("td");
   let read = document.createElement("td");
-  let read_span = document.createElement("span");
+  let readSpan = document.createElement("span");
   let trash = document.createElement("td");
-  let trash_icon = document.createElement("i");
+  let trashIcon = document.createElement("i");
   
   // setting the text
   title.textContent = this.title;
   author.textContent = this.author;
   pages.textContent = this.pages;
-  read_span.textContent = this.read ? "yes" : "no";
+  readSpan.textContent = this.read ? "yes" : "no";
   
   // adding classes
   row.classList.add("row");
   row.classList.add("row--content");
-  trash_icon.classList.add("fa");
-  trash_icon.classList.add("fa-trash");
-  trash_icon.classList.add("del");
-  read_span.classList.add("read");
-  read_span.classList.add(this.read ? "green-hover" : "red-hover");
+  trashIcon.classList.add("fa");
+  trashIcon.classList.add("fa-trash");
+  trashIcon.classList.add("del");
+  readSpan.classList.add("read");
+  readSpan.classList.add(this.read ? "green-hover" : "red-hover");
 
+  // adding data attribute
+  row.setAttribute("data-id", this.id);
+  readSpan
   // adding event listener
-  trash_icon.addEventListener("click", removeBook);
+  trashIcon.addEventListener("click", removeBook);
+  readSpan.addEventListener("click", toggleRead);
 
   // appending childs
-  read.appendChild(read_span); 
-  trash.appendChild(trash_icon);
+  read.appendChild(readSpan); 
+  trash.appendChild(trashIcon);
   row.appendChild(title);
   row.appendChild(author);
   row.appendChild(pages);
@@ -81,10 +94,32 @@ Library.prototype.renderAllBooks = function(tableBody) {
   this.books.forEach(book => book.render(tableBody));
 }
 
+Library.prototype.getNextId = function() {
+  let nextId = 0;
+  let len = this.books.length;
+  if (len > 0) {
+    let lastId = parseInt(this.books[this.books.length - 1].id);
+    nextId = lastId + 1;
+  }
+  return nextId;
+}
 
+Library.prototype.removeBookById = function(id) {
+  let idArrays = this.books.map(book => book.id);
+  let idx = idArrays.indexOf(id);
+  this.books.splice(idx, 1);
+  return idx;
+}
+
+Library.prototype.toggleReadStatusOfABookById = function(id) {
+  let idArrays = this.books.map(book => book.id);
+  let idx = idArrays.indexOf(id);
+  this.books[idx].toggleReadStatus();
+  return idx;
+}
 
 // ==== PAGE ====
-// elements
+// Variables
 let library = new Library();
 const tableBody = document.querySelector(".table").querySelector("tbody");
 
@@ -101,7 +136,7 @@ const newBookForm = {
   read: document.querySelector("#read-checkbox")
 }
 
-// METHODS
+// Functions
 let setEventListeners = function() {
   addBookBtns.forEach(btn => {
     btn.addEventListener("click", openDialog);
@@ -125,6 +160,7 @@ let closeDialog = function() {
 
 let saveBook = function() {
   // get input values
+  let id = library.getNextId();
   let title = newBookForm["title"].value;
   let author = newBookForm["author"].value;
   let pages = newBookForm["pages"].value;
@@ -136,15 +172,25 @@ let saveBook = function() {
   if (!validatePages(pages)) return;
 
   // create a new book and add to library
-  let newBook = new Book(title, author, pages, read);
+  let newBook = new Book(id, title, author, pages, read);
   library.addBook(newBook);
   library.renderAllBooks(tableBody);
   closeDialog();
 }
 
 let removeBook = function(e) {
-  console.log(e.target.parentElement.parentElement);
-} 
+  let row = (e.target.parentElement.parentElement);
+  let id = parseInt(row.getAttribute("data-id"));
+  let bookIdx = library.removeBookById(id);
+  library.renderAllBooks(tableBody);
+}
+
+let toggleRead = function(e) {
+  let row = (e.target.parentElement.parentElement);
+  let id = parseInt(row.getAttribute("data-id"));
+  let bookIdx = library.toggleReadStatusOfABookById(id);
+  library.renderAllBooks(tableBody);
+}
 
 let validateTitle = function(title) {
   if (title === "") {
@@ -181,5 +227,5 @@ let validatePages = function(pages) {
 
 // INITIALIZING
 setEventListeners();
-library.addBook(new Book("Sapiens", "Yuval Noah Harari", "443", true));
+library.addBook(new Book(1, "Sapiens", "Yuval Noah Harari", "443", true));
 library.renderAllBooks(tableBody);
